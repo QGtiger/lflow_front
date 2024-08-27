79233ab8973bdb5058744262d96415f15d353b17
光鱼 <guang.yu@yingdao.com>
2024-08-27 20:29:20 +0800

    feat: 文件路由配置添加

---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+export default function IndexPage() {
+  console.log(11)
+  return 11
+}
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      height: {
+        "screen-1/2": "50vh",
+        "screen-1/3": "33.333333vh",
+        "screen-2/3": "66.666667vh",
+        "screen-1/4": "25vh",
+        "screen-3/4": "75vh",
+        screen: "100vh",
+      },
+      width: {
+        screen: "100vw",
+      },
+      textColor: {
+        labelFaint: "#9CA3AF",
+        labelMuted: "#6b7076",
+      },
+      backgroundColor: {
+        controlSelected: "#F3F4F6",
+      },
+    },
+  },
+  plugins: [],
+};
