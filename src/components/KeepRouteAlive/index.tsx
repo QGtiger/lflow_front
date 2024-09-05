6de0e4949463cc19f23d0b9c558796902909070d
光鱼 <guang.yu@yingdao.com>
2024-09-05 17:59:05 +0800

    feat: 构建不用校验ts

--- src/components/KeepRouteAlive/index.tsx
+++ src/components/KeepRouteAlive/index.tsx
@@ -1,23 +1,22 @@
-import { PropsWithChildren, useEffect } from "react";
-import KeepAlive, { useAliveController } from "react-activation";
+import { PropsWithChildren } from "react";
+import KeepAlive from "react-activation";
 import { useLocation } from "react-router-dom";
 
 export default function KeepRouteAlive({
   children,
-  lisenSearch,
 }: PropsWithChildren<{
   lisenSearch?: boolean;
 }>) {
   const { pathname } = useLocation();
-  const { drop, getCachingNodes, refresh, refreshScope } = useAliveController();
+  // const { drop, getCachingNodes, refresh, refreshScope } = useAliveController();
 
-  useEffect(() => {
-    window.drop = drop;
-    window.getCachingNodes = getCachingNodes;
-    window.refresh = refresh;
-    window.refreshScope = refreshScope;
-    console.log(pathname);
-  }, []);
+  // useEffect(() => {
+  //   window.drop = drop;
+  //   window.getCachingNodes = getCachingNodes;
+  //   window.refresh = refresh;
+  //   window.refreshScope = refreshScope;
+  //   console.log(pathname);
+  // }, []);
 
   return (
     <KeepAlive cacheKey={pathname} name={pathname}>
