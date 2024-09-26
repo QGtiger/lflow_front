b61f36fbe9cacedd416430a9f0535769fbb73e04
光鱼 <guang.yu@yingdao.com>
2024-09-26 17:39:06 +0800

    feat: qiankun 接入 ipaas

---
+++ src/pages/ipaas/index.tsx
@@ -0,0 +1,18 @@
+import { useMount } from "ahooks";
+import { registerMicroApps, start } from "qiankun";
+
+export default function IPaaS() {
+  useMount(() => {
+    registerMicroApps([
+      {
+        name: "micro-ipaas",
+        entry: process.env.MICRO_IPASS_URL!,
+        container: "#ipaas-container",
+        activeRule: "/ipaas",
+      },
+    ]);
+    start();
+  });
+
+  return <div id="ipaas-container"></div>;
+}
---
+++ src/pages/ipaas/settings.tsx
@@ -0,0 +1,8 @@
+import { CloudFilled } from "@ant-design/icons";
+
+export default {
+  menuConfig: {
+    name: "iPaaS",
+    icon: <CloudFilled />,
+  },
+} as PageSettings;
