4659121e763f8b9481ceb750feb39915feae85f6
光鱼 <guang.yu@yingdao.com>
2024-10-08 09:50:40 +0800

    feat: 微应用loading 添加

--- src/pages/ipaas/MicroIpassApp.tsx
+++ src/pages/ipaas/MicroIpassApp.tsx
@@ -1,24 +1,48 @@
 import { request } from "@/api/request";
 import { useMount } from "ahooks";
 import { registerMicroApps, start } from "qiankun";
+import { useRef, useState } from "react";
+import { Skeleton } from "antd";
 
 import "./index.css";
 
 export default function MicroIPaaS() {
+  const skeletonRef = useRef<HTMLDivElement>(null);
+
   useMount(() => {
-    registerMicroApps([
-      {
-        name: "micro-ipaas",
-        entry: process.env.MICRO_IPASS_URL!,
-        container: "#ipaas-container",
-        activeRule: "/ipaas",
-        props: {
-          requestClient: request,
+    registerMicroApps(
+      [
+        {
+          name: "micro-ipaas",
+          entry: process.env.MICRO_IPASS_URL!,
+          container: "#ipaas-container",
+          activeRule: "/ipaas",
+          props: {
+            requestClient: request,
+          },
         },
-      },
-    ]);
+      ],
+      {
+        async afterMount() {
+          const skeletonContainer: any = document.querySelector(
+            ".skeleton-container"
+          );
+          console.log("afterMount", skeletonContainer);
+          if (skeletonContainer) {
+            skeletonContainer.style.display = "none";
+          }
+        },
+      }
+    );
     start();
   });
 
-  return <div id="ipaas-container" className="h-full"></div>;
+  return (
+    <>
+      <div className="p-4 skeleton-container" ref={skeletonRef}>
+        <Skeleton active />
+      </div>
+      <div id="ipaas-container" className="h-full"></div>
+    </>
+  );
 }
